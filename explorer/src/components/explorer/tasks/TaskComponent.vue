aae71c431dd86185ab59e03eea276827662a5c25
Peter Heick <peter@heick.dk>
2023-04-21 10:37:33 +0200

    Save før større ændring

--- explorer/src/components/explorer/tasks/TaskComponent.vue
+++ explorer/src/components/explorer/tasks/TaskComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="taskNode.name === shownNode">
-    <table role="presentation" cellspacing="0" cellpadding="3" border="0">
+    <table v-show="false" role="presentation" cellspacing="0" cellpadding="3" border="0">
       <colgroup>
         <col width="305" />
         <col width="306" />
@@ -18,19 +18,26 @@
         </tr>
         <tr>
           <td align="left" valign="top">
-            <span id="isc_X6" class="formTitle" align="left">
+            <span
+              id="isc_X6"
+              class="formTitle"
+              align="left"
+              style="display: inline-block"
+            >
               <label id="isc_X8" for="isc_X7"> Name </label>&nbsp;
               <span style="color: red">*</span>
             </span>
             <br />
-            <span id="td-name" class="text">{{ taskNode.name }}</span>
+            <span id="td-name" class="text uc-field">{{ taskNode.name }}</span>
           </td>
           <td align="left" valign="top">
             <span align="left">
               <label id="isc_183">Version</label>
             </span>
             <br />
-            <span id="td-version" class="text">{{ taskNode.version }}</span>
+            <span id="td-version" class="text uc-field" style="width: 72px">{{
+              taskNode.version
+            }}</span>
           </td>
         </tr>
         <tr>
@@ -39,7 +46,7 @@
               <label id="isc_XC" for="isc_XB"> Description</label>
             </span>
             <br />
-            <span id="td-description" class="text">
+            <span id="td-description" class="text uc-field">
               {{ taskNode.summary }}
             </span>
           </td>
@@ -50,26 +57,29 @@
               <label id="isc_XF"> Member of Business Services</label>
             </span>
             <br />
-            <span
-              class="text"
-              v-for="(bs, index) in taskNode.opswiseGroups"
-              :key="index"
-              > {{ bs }} </span
-            >
+            <div class="text uc-field" style="width: 100%">
+              <span style="padding-right: 9px;" v-for="(bs, index) in taskNode.opswiseGroups" :key="index">
+                {{ bs }}
+              </span>
+            </div>
           </td>
         </tr>
       </tbody>
     </table>
+    <GeneralComponent :taskNode="taskNode"></GeneralComponent>
+    <AgentComponent :taskNode="taskNode"></AgentComponent>
     <TaskUnixComponent :taskNode="taskNode"></TaskUnixComponent>
   </div>
 </template>
 <script lang="ts">
 import { ItreeNode } from "@/types/interfaces";
 import { defineComponent } from "vue";
+import GeneralComponent from "./GeneralComponent.vue";
 import TaskUnixComponent from "./TaskUnixComponent.vue";
+import AgentComponent from"./AgentComponent.vue";
 
 export default defineComponent({
-  components: { TaskUnixComponent },
+  components: { GeneralComponent, TaskUnixComponent, AgentComponent },
   props: {
     taskNode: {
       type: Object as () => ItreeNode,
@@ -87,3 +97,6 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+</style>
